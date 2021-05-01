a94a5da87a1de78fca558279127474502fecc413
Grandayyyy <grandy.maddhav18@st.niituniversity.in>
2021-05-02 01:19:40 +0530

    edited some stuff

--- django_project_blog/settings.py
+++ django_project_blog/settings.py
@@ -139,13 +139,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
