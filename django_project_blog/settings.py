6ff32cf4db6eda69a09cbc4c648f18ee43eb0fff
Grandayyyy <grandy.maddhav18@st.niituniversity.in>
2021-05-01 21:28:33 +0530

    second commit

--- django_project_blog/settings.py
+++ django_project_blog/settings.py
@@ -143,8 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'maddhavgrandy@gmail.com'
+EMAIL_HOST_PASSWORD = 'sranfgsgiityothz'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
